099cd545fc883f4f46f2f110397698836c8b8a0f
rafalstrozyk <rafal.strozyk.biz@gmail.com>
2020-06-03 15:40:40 +0200

    Home header 01

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,18 +1,22 @@
 import React from 'react';
+import headerImage from '../../assets/images/header1.png';
 
-import {StyledHome} from './Home.style';
+import { StyledHome, StyledTitle, StyleHeaderPhoto } from './Home.style';
 
 const home = (props) => {
-
-    return (
-        <StyledHome>
-            <div>
-                <h1>Rafał Stróżyk</h1>
-                <span><h1>Front-end Developer</h1></span>
-            </div>
-            
-        </StyledHome>
-    )
-}
+	return (
+		<StyledHome>
+			<StyledTitle>
+				<h1>Rafał Stróżyk</h1>
+				<span>
+					<h1>Front-end Developer</h1>
+				</span>
+			</StyledTitle>
+			<StyleHeaderPhoto>
+				<img src={headerImage} alt='Rafał Stróżyk' />
+			</StyleHeaderPhoto>
+		</StyledHome>
+	);
+};
 
 export default home;
